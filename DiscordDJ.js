e95aef8edf01f72d31e6747b9317e4ae24f0b912
Guichaguri <guichaguri@hotmail.com>
2015-11-28 19:27:17 -0200

    Cleanup

--- DiscordDJ.js
+++ DiscordDJ.js
@@ -9,8 +9,153 @@ var DJ = require("./lib/Logic/DJ.js");
 
 var YoutubeVideo = require("./lib/Audio/YoutubeVideo.js");
 var FileAudio = require("./lib/Audio/FileAudio.js");
+var IcyAudio = require("./lib/Audio/IcyAudio.js");
 
 var bots = require('./bots.json');
+
+var commands = {
+    add: {
+        alias: ["add", "addsong", "addvideo", "youtube"],
+        run: function(bot, dj, user, msg, args) {
+            try {
+                dj.addToQueue(new YoutubeVideo(user, args[1]));
+                bot.deleteMessage(msg);
+            } catch (e) {
+                console.log("Invalid URL: " + args[1]);
+            }
+        }
+    },
+    skip: {
+        alias: ["skip", "stop"],
+        run: function(bot, dj, user, msg, args) {
+            var roles = msg.channel.server.rolesOfUser(user);
+            var b = false;
+            roles.forEach(function(role) {
+                if(role.hasPermission("manageMessages")) b = true;
+            });
+            if(!b) return;
+            dj.skip();
+            bot.deleteMessage(msg);
+        }
+    }
+};
+
+class DiscordDJ {
+
+    constructor(bot) {
+        this.bot = bot;
+        this.dj = new DJ(bot);
+
+        this.nowPlayingPrefix = "";
+        this.watchChat = null;
+        this.logChat = null;
+        this.playingMessage = null;
+        this.playingMessageObj = null;
+
+        var self = this;
+        bot.on("message", function(msg) {
+            self.onCommand(msg);
+        });
+        this.dj.on("data-changed", function() {
+            self.updatePlayableData();
+        });
+        this.dj.on("play", function() {
+            self.playingMessage = self.createPlayingMessage(self.dj.playable);
+            self.log(self.nowPlayingPrefix + self.playingMessage, function(error, message) {
+                if(error == null) {
+                    self.playingMessageObj = message;
+                }
+            });
+        });
+        this.dj.on("skip", function() {
+            if(self.playingMessageObj != null) {
+                self.bot.updateMessage(this.playingMessageObj, this.playingMessage);
+                self.playingMessageObj = null;
+                self.playingMessage = null;
+            }
+        });
+    }
+
+    onCommand(msg) {
+        if(msg.author.id == this.bot.user.id) return;
+        if(!(msg.channel instanceof Discord.ServerChannel)) return;
+        if(this.bot.voiceConnection != null && this.bot.voiceConnection.voiceChannel.server != msg.channel.server) return;
+        if(this.watchChat == null || msg.channel == this.watchChat) {
+            if(msg.content[0] == '/' || msg.content[0] == '!') {
+                var args = msg.content.split(" ");
+                var label = args[0].substring(1).toLowerCase();
+                for(var cmd in commands) {
+                    if(commands[cmd].alias.contains(label)) {
+                        commands[cmd].run(this.bot, this.dj, msg.author, msg, args);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    watchForChannel(ch) {
+        if(ch instanceof String || typeof ch == String) {
+            this.bot.channels.forEach(function(channel) {
+                if(!(channel instanceof Discord.TextChannel)) return;
+                if(channel.id == ch || channel.name == ch) {
+                    ch = channel;
+                }
+            });
+        }
+        this.watchChat = ch;
+    }
+
+    logIntoChannel(ch) {
+        if(ch instanceof String || typeof ch == String) {
+            this.bot.channels.forEach(function(channel) {
+                if(!(channel instanceof Discord.TextChannel)) return;
+                if(channel.id == ch || channel.name == ch) {
+                    ch = channel;
+                }
+            });
+        }
+        this.logChat = ch;
+    }
+
+    setMessagePrefix(prefix) {
+        this.nowPlayingPrefix = prefix;
+    }
+
+    createPlayingMessage(playable) {
+        var info = playable.getAdditionalInfo();
+        return playable.getTitle() + "\n" + (playable.user != null ? ("by " + playable.user.mention()) : "") +
+                (info != null ? " " + info : "");
+    }
+
+    updatePlayableData() {
+        if(this.playable == null) return;
+        if(this.playingMessageObj == null) return;
+        var newMsg = this.createPlayingMessage(this.playable);
+        if(this.playingMessage != newMsg) {
+            if(this.playable instanceof IcyAudio) {
+                this.bot.updateMessage(this.playingMessageObj, this.playingMessage); // Remove prefix
+                var self = this;
+                this.log(this.nowPlayingPrefix + newMsg, function(error, message) {
+                    if(error == null) {
+                        self.playingMessageObj = message;
+                    }
+                });
+            } else {
+                this.bot.updateMessage(this.playingMessageObj, this.nowPlayingPrefix + newMsg);
+            }
+            this.playingMessage = newMsg;
+        }
+    }
+
+    log(msg, chatCallback) {
+        console.log(msg);
+        if(this.logChat != null) {
+            this.bot.sendMessage(this.logChat, msg, {}, chatCallback);
+        }
+    }
+
+}
 
 bots.forEach(function(data) {
 
@@ -20,7 +165,8 @@ bots.forEach(function(data) {
     }
 
     var bot = new Discord.Client();
-    var dj = new DJ(bot);
+    var dj = new DiscordDJ(bot);
+
     bot.login(data.email, data.password, function (error) {
         if (error != null) {
             console.log(error);
@@ -29,14 +175,19 @@ bots.forEach(function(data) {
     });
 
     bot.on("ready", function () {
-        dj.joinVoiceChat(data.voice, function(error) {
-            if (error != null) {
+
+        // Join Voice Channel
+        dj.dj.joinVoiceChat(data.voice, function(error) {
+            if(error != null) {
                 console.log(error);
                 process.exit(2);
             }
         });
-        dj.watchTextChat(data.text);
-        dj.logIntoTextChat(data.log);
+
+        dj.watchForChannel(data.text);
+        dj.logIntoChannel(data.log);
+        dj.setMessagePrefix(data.prefix || "**Now Playing:** ")
+
     });
 
 });
--- README.md
+++ README.md
@@ -3,21 +3,22 @@ Discord DJ Bot. Let you play music in your server. Inspired by PlugDJ
 
 ## Installing
 To install DiscordDJ, you just need [Node.js and NPM](http://nodejs.org) and run the command below:
+
 ```npm install discord-dj```
 
 ## Configuring
-After you have installed DiscordDJ, open `bots.json`
-**email:** The email for the discord account of the bot
-**password:** The password for the discord account of the bot
-**voice:** The voice channel name or ID that the bot should join
-**log:** The text channel where the bot will send "now playing" messages. Setting this to null will disable log messages
-**text:** The text channel where the bot will watch for commands. Setting this to null will make it work for all text channels
+After you have installed DiscordDJ, open `bots.json` (it should be in `node_modules/discord-dj`)
+
+* **email:** The email for the discord account of the bot
+* **password:** The password for the discord account of the bot
+* **voice:** The voice channel name or ID that the bot should join
+* **log:** The text channel where the bot will send "now playing" messages. Setting this to null will disable log messages
+* **text:** The text channel where the bot will watch for commands. Setting this to null will make it work for all text channels
+
 You can add more entries to run more bots in the same server
 
 ## Running
-To run DiscordDJ, you need to execute one of these commands:
-```npm start```
-```node DiscordDJ.js```
+To run DiscordDJ, you need to execute `npm start` or `node DiscordDJ.js` inside `node_modules/discord-dj`
 
 ## Developing
 If you are a developer and want DiscordDJ as a library, you can:
@@ -31,8 +32,9 @@ var dj = new DiscordDJ.DJ(bot); // bot is a discord.js Client object
 // Add a song to queue (user is a discord.js User object)
 dj.addToQueue(new DiscordDJ.FileAudio(user, "path/to/audio.mp3"));
 dj.addToQueue(new DiscordDJ.YoutubeVideo(user, "https://www.youtube.com/watch?v=CwW6l29vvjI"));
+dj.addToQueue(new DiscordDJ.IcyAudio(user, "http://listen.radionomy.com/DeepHouse")); // Shoutcast or Icecast stream. It will only stop playing if the radio server is shutdown
 
-// Skip current song
+// Skip/stop current song
 dj.skip();
 
 // Force another song to play
